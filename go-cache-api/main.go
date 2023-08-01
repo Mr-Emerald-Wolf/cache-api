5b692f0a3549807d38187232d1f29748dab44c9d
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2023-08-01 22:18:48 +0530

    feat: logger added

--- go-cache-api/main.go
+++ go-cache-api/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+
 	"github.com/mr-emerald-wolf/go-cache-api/routes"
 )
 
 func main() {
 
 	app := fiber.New()
+	app.Use(logger.New())
 
 	routes.FifoRoutes(app)
 	routes.LifoRoutes(app)
